ece4af17f78300c02ea3397c6c1c2003f648be8d
jeneferjene <38182531+jeneferjene@users.noreply.github.com>
2019-07-11 02:17:29 -0700

    Update evenodd.py

--- evenodd.py
+++ evenodd.py
@@ -1,9 +1,11 @@
-in_num = input()
-try:
-  inp_num=int(in_num)
-  if(inp_num%2)==0:
-    print("Even")
-  else:
-    print("Odd")
-except ValueError:
+ch = input("")
+if((ch >= 'a' and ch <= 'z') or (ch >= 'A' and ch <= 'Z')): 
+    print("Invalid") 
+elif(ch >= '0' and ch <= '9'):
+    num=int(ch)
+    if (num % 2) == 0:
+        print("Even")
+    else:
+        print("Odd")
+else:
     print("Invalid")
